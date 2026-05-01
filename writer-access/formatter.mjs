e0763f1df5066be34ac3da77094535c46ad07361
tomrcc <tom@cloudcannon.com>
2026-05-01 14:27:37 +1200

    Add missing frontmatter fields for blog posts

--- writer-access/formatter.mjs
+++ writer-access/formatter.mjs
@@ -68,6 +68,15 @@ function jsonToMdx(postData) {
     },
     thumb_image_path: "",
     thumb_image_alt: "",
+    seo: {
+      page_description: "",
+      canonical_url: "",
+      featured_image: "",
+      featured_image_alt: "",
+      author_twitter_handle: "",
+      open_graph_type: "article",
+      no_index: false,
+    },
     ...rest,
   };
 
